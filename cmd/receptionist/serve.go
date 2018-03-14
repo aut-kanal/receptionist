88dd74f71ba0cf3a3bfc18717256c492eee0a65f
Arya Hadi <arya.hadi97@gmail.com>
2018-03-14 10:38:40 +0330

    chore(cmd): add `serve` command

--- cmd/receptionist/serve.go
+++ cmd/receptionist/serve.go
@@ -1,1 +1,17 @@
 package main
+
+import "github.com/spf13/cobra"
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start server",
+	Run:   serve,
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
+
+func serve(cmd *cobra.Command, args []string) {
+	logVersion()
+}
